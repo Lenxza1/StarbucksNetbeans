fb701bf0188edc1ab9de4aa582f4ea6f66e21624
Jars31 <fajarsetiawan31122004@gmail.com>
2022-10-10 09:06:55 +0700

    wio

--- src/koneksi/Koneksi.java
+++ src/koneksi/Koneksi.java
@@ -3,7 +3,7 @@ package koneksi;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
-
+    
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,5 +14,10 @@ import javax.swing.JOptionPane;
  * @author naufa
  */
 public class Koneksi {
-    
+    Connection koneksi=null;
+    public static Connection konek(){
+        try{
+            
+        }
+    }
 }
